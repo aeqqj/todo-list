b9fd552f4a0691df576e3fee168c30faf038e968
aeqqj <48713075+aeqqj@users.noreply.github.com>
2025-04-09 18:02:17 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById("input");
+const listContainer = document.getElementById("list");
+
+function addTask() {
+    console.log("oten");
+    if(inputBox.value === "") {
+        alert("You must write something!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    input.value =  "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(x) {
+    if (x.target.tagName === "LI") {
+        x.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (x.target.tagName === "SPAN") {
+        x.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showList() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showList();
